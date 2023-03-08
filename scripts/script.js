906d382fdd9a09d51cb7de3157fa32ba8051bb35
Makarov <joseignaciosajnovsky.9@gmail.com>
2023-03-08 20:33:38 -0300

    first commit ticmas

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+document.getElementById('botonproject').onclick = function() {
+    
+    
+    
+    document.getElementById('projects').style.display = 'block';
+
+    document.getElementById('botonproject').style.display = 'none';
+}
